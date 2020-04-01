5979ce9d100a4febfaf786037de6edbfbc4bc8dc
DougIT <doug@proeditors.com>
2020-03-31 21:40:01 -0600

    Add project files.

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,26 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+  [ApiController]
+  public class AuthorsController : ControllerBase
+  {
+    private readonly ICourseLibraryRepository courseLibraryRepository;
+
+    public AuthorsController(ICourseLibraryRepository courseLibraryRepository) {
+      this.courseLibraryRepository = courseLibraryRepository ??
+        throw new ArgumentNullException(nameof(courseLibraryRepository));
+    }
+
+    public IActionResult GetAuthors() {
+      var authorsFromRepo = courseLibraryRepository.GetAuthors();
+      return new JsonResult(authorsFromRepo);
+
+    }
+  }
+}
