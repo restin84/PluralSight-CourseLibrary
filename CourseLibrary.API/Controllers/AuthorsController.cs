02ed16a503616b13627b96430dec9cfa15fb5b08
DougIT <doug@proeditors.com>
2020-04-04 16:03:13 -0600

    Video: Getting a Single Resource

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 namespace CourseLibrary.API.Controllers
 {
   [ApiController]
+  [Route("api/authors")]
   public class AuthorsController : ControllerBase
   {
     private readonly ICourseLibraryRepository courseLibraryRepository;
@@ -17,10 +18,16 @@ namespace CourseLibrary.API.Controllers
         throw new ArgumentNullException(nameof(courseLibraryRepository));
     }
 
+    [HttpGet]
     public IActionResult GetAuthors() {
       var authorsFromRepo = courseLibraryRepository.GetAuthors();
       return new JsonResult(authorsFromRepo);
+    }
 
+    [HttpGet("{authorId}")]
+    public IActionResult GetAuthor(Guid authorId) {
+      var authorFromRepo = courseLibraryRepository.GetAuthor(authorId);
+      return new JsonResult(authorFromRepo);
     }
   }
 }
