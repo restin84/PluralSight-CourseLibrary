8ecb97f89a8f375494539bfb0deeff310a5a7930
DougIT <doug@proeditors.com>
2020-04-08 21:25:08 -0600

    Video: Separating Entity Model and Outer Facing Model

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -1,4 +1,6 @@
-﻿using CourseLibrary.API.Services;
+﻿using CourseLibrary.API.Helpers;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -21,7 +23,18 @@ namespace CourseLibrary.API.Controllers
     [HttpGet]
     public IActionResult GetAuthors() {
       var authorsFromRepo = courseLibraryRepository.GetAuthors();
-      return Ok(authorsFromRepo);
+      var authors = new List<AuthorDto>();
+
+      foreach(var author in authorsFromRepo) {
+        authors.Add(new AuthorDto() {
+          Id = author.Id,
+          Name = $"{author.FirstName} {author.LastName}",
+          MainCategory = author.MainCategory,
+          Age = author.DateOfBirth.GetCurrentAge()
+        });
+      }
+
+      return Ok(authors);
     }
 
     [HttpGet("{authorId}")]
