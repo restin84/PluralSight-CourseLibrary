514e286b91f9ee748f8c8aa13d0bbc13a7ed5d0e
Douglas I Tait <restin84@gmail.com>
2020-06-15 21:22:12 -0600

    Video: Creating a Collection of Resources

---
+++ CourseLibrary.API/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Entities;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+  [ApiController]
+  [Route("api/authorcollections")]
+  public class AuthorCollectionsController : ControllerBase
+  {
+    private readonly ICourseLibraryRepository courseLibraryRepository;
+    private readonly IMapper mapper;
+
+    public AuthorCollectionsController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper) {
+      this.courseLibraryRepository = courseLibraryRepository ??
+        throw new ArgumentNullException(nameof(courseLibraryRepository));
+      this.mapper = mapper ??
+        throw new ArgumentNullException(nameof(mapper));
+    }
+
+    [HttpPost]
+    public ActionResult<IEnumerable<AuthorDto>> CreateAuthorCollection(
+      IEnumerable<AuthorForCreationDto> authorCollection) {
+
+      var authorEntities = mapper.Map<IEnumerable<Author>>(authorCollection);
+
+      foreach(var author in authorEntities) {
+        courseLibraryRepository.AddAuthor(author);
+      }
+
+      courseLibraryRepository.Save();
+
+      return Ok();
+    }
+  }
+}
