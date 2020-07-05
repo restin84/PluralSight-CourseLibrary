d06527a478c00119fcfe58d14b22fa06b6a67db0
Douglas I Tait <restin84@gmail.com>
2020-07-05 09:27:10 -0600

    Video: Validating Input when Updating a Resource with PUT

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -7,22 +7,7 @@ using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Models
 {
-  [CourseTitleMustBeDifferentFromTheDescription]
-  public class CourseForCreationDto // : IValidatableObject
+  public class CourseForCreationDto : CourseForManipulationDto
   {
-    [Required]
-    [MaxLength(100)]
-    public string Title { get; set; }
-
-    [MaxLength(1500)]
-    public string Description { get; set; }
-
-    //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) {
-    //  if (Title == Description) {
-    //    yield return new ValidationResult(
-    //      "The provided description should be different from the title",
-    //      new[] { "CourseForCreationDto" });
-    //  }
-    //}
   }
 }
--- CourseLibrary.API/ValidationAttributes/CourseTitleMustBeDifferentFromTheDescriptionAttribute.cs
+++ CourseLibrary.API/ValidationAttributes/CourseTitleMustBeDifferentFromTheDescriptionAttribute.cs
@@ -13,12 +13,12 @@ namespace CourseLibrary.API.ValidationAttributes
       //in this case we are validating at the class level so object and validationContext refer to the same thing
       //if we were instead validating a property then value would refer to the object that the property we 
       //are validating belongs to and validationContext will refer to the property that we are validating
-      var course = (CourseForCreationDto)validationContext.ObjectInstance;
+      var course = (CourseForManipulationDto)validationContext.ObjectInstance;
 
       if (course.Title == course.Description) {
         return new ValidationResult(
             "The provided desciption should be different from the title.",
-            new [] {nameof(CourseForCreationDto)}
+            new [] {nameof(CourseForManipulationDto) }
           );
       }
 
